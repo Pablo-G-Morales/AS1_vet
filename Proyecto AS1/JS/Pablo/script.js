4966f16ab3cbf15927302145cf7c7d38ff2bcd85
Pablo-G-Morales <84752203+Pablo-G-Morales@users.noreply.github.com>
2024-05-04 22:01:02 -0600

    Add files via upload

---
+++ Proyecto AS1/JS/Pablo/script.js
@@ -0,0 +1,18 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("slides")[0].children;
+    if (n >= slides.length) {
+        slideIndex = 0;
+    } else if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.transform = `translateX(-${slideIndex * 100}%)`;
+    }
+}
